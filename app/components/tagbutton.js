590aafc6c5ecd9c4bbd6a7231a0fff97fa7aced9
adarshgupta404 <gadarsh747@gmail.com>
2023-09-18 15:26:54 +0530

    added search functionality

--- app/components/tagbutton.js
+++ app/components/tagbutton.js
@@ -1,19 +1,11 @@
 "use client";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Ripple, initTE } from "tw-elements";
 import { useRouter } from "next/navigation";
 import { useDebounce } from "use-debounce";
 const Button = ({text}) => {
-  const [input, setinput] = useState("");
-  const handleclick = (tag) => {
-    // setinput(tag.charAt(0).toLowerCase() + input.slice(1));
-    setinput(tag.toLowerCase());
-    console.log(input)
-    // console.log(converted)
-  };
   const router = useRouter();
-  const [query] = useDebounce(input, 200)
+  const [query] = useDebounce(text, 200)
   useEffect(()=>{
     if(!query){
         router.push('/blog')
@@ -27,7 +19,6 @@ const Button = ({text}) => {
   return (
     <button
       type="button"
-      onClick={()=>handleclick(text)}
       data-te-ripple-init
       data-te-ripple-color="white"
       className="text-black focus:duration-500 ease-in-out focus:text-white bg-gray-100 bg-gradient-to-r focus:bg-purple-400 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2"
