c0d7cd8ae6eeadc398900f09b09d320056a72b84
adarshgupta404 <gadarsh747@gmail.com>
2023-09-18 19:03:57 +0530

    inor change

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -167,7 +167,7 @@ const SimpleSlider = () => {
             data-te-ripple-color="white"
             className="text-black focus:duration-500 ease-in-out focus:text-white bg-gray-100 bg-gradient-to-r focus:bg-purple-400 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2"
           >
-            Decor
+            Regional
           </button>
         </div>
       </div>
