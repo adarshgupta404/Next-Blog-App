0467554d7e11e0cd4ee6c30441840eabe28bd2a7
adarshgupta404 <gadarsh747@gmail.com>
2023-09-18 16:21:10 +0530

    added search functionality

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -11,6 +11,7 @@ import { useDebounce } from "use-debounce";
 const SimpleSlider = () => {
   const sliderRef = useRef(0);
   useEffect(() => {
+    router.push("/blog");
     initFlowbite();
   }, []);
   const next = () => {
